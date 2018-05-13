a410295d6c83a32081e3cb4323cc379be3e96e8a
Cara Leitheiser <caraleitheiser@hotmail.com>
2018-05-13 00:34:53 -0500

    final updates
    icon legend added, other updates

--- js/index.js
+++ js/index.js
@@ -9,6 +9,21 @@ $(document).ready(function() {
         $('#creditimg').fadeOut(500);
         $('#creditclose').fadeOut(500);
     });
+    
+    $('#iconselect').click(function() {
+        $('#iconlegend').fadeIn(500);
+        $('#legendclose').fadeIn(500);
+        $('.container').fadeOut('fast');
+        $('.navbar').fadeOut('fast');
+        
+    });
+    
+    $('#legendclose').click(function() {
+        $('#iconlegend').fadeOut(500);
+        $('#legendclose').fadeOut(500);
+        $('.container').fadeIn('fast');
+        $('.navbar').fadeIn('fast');
+    });
       
     $('.container').click(function() {
             $('.navbar').fadeToggle(400);
